9380dfa6dd8a16676cf174f5adb9de4bb01554f9
Kacper Leśniański <kacper.lesnianski@wp.pl>
2022-12-11 15:44:51 +0100

    day9p2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -37,160 +37,135 @@ fn dist(p1: &(i32, i32), p2: &(i32, i32)) -> i32 {
     return len;
 }
 
-fn move_left(head_pos: &mut (i32, i32), tail_pos: &mut (i32, i32), d: i32) -> Vec<(i32, i32)> {
-    let mut visited = vec![];
-    head_pos.0 -= d;
-    let distance = dist(head_pos, tail_pos);
-    if distance > 1 {
-        for i in head_pos.0 + 1..tail_pos.0 {
-            visited.push((i, head_pos.1));
-        }
-        tail_pos.0 = head_pos.0 + 1;
-        tail_pos.1 = head_pos.1;
+fn follow(head: &mut (i32, i32), tail: &mut (i32, i32)) {
+    let distance = dist(head, tail);
+    if distance <= 1 {
+        return;
     }
 
-    return visited;
+    if head.0 == tail.0 {
+        if head.1 > tail.1 {
+            tail.1 += 1;
+        } else {
+            tail.1 -= 1;
+        }
+    } else if head.1 == tail.1 {
+        if head.0 > tail.0 {
+            tail.0 += 1;
+        } else {
+            tail.0 -= 1;
+        }
+    } else if head.0 > tail.0 {
+        tail.0 += 1;
+        if head.1 > tail.1 {
+            tail.1 += 1;
+        } else {
+            tail.1 -= 1;
+        }
+    } else {
+        tail.0 -= 1;
+        if head.1 > tail.1 {
+            tail.1 += 1;
+        } else {
+            tail.1 -= 1;
+        }
+    }
 }
 
-fn move_right(head_pos: &mut (i32, i32), tail_pos: &mut (i32, i32), d: i32) -> Vec<(i32, i32)> {
-    let mut visited = vec![];
-    head_pos.0 += d;
-    let distance = dist(head_pos, tail_pos);
-    if distance > 1 {
-        for i in tail_pos.0 + 1..head_pos.0 {
-            visited.push((i, head_pos.1));
-        }
-        tail_pos.0 = head_pos.0 - 1;
-        tail_pos.1 = head_pos.1;
+fn follow_chain(points_pos: &mut [(i32, i32)]) {
+    for i in 1..points_pos.len() {
+        let mut head = points_pos[i - 1];
+        let mut tail = points_pos[i];
+        follow(&mut head, &mut tail);
+        points_pos[i - 1] = head;
+        points_pos[i] = tail;
     }
-
-    return visited;
 }
 
-fn move_up(head_pos: &mut (i32, i32), tail_pos: &mut (i32, i32), d: i32) -> Vec<(i32, i32)> {
-    let mut visited = vec![];
-    head_pos.1 += d;
-    let distance = dist(head_pos, tail_pos);
-    if distance > 1 {
-        for i in tail_pos.1 + 1..head_pos.1 {
-            visited.push((head_pos.0, i));
+fn move_v2(points_pos: &mut [(i32, i32)], mv: (Direction, i32)) -> Vec<(i32, i32)> {
+    let mut tail_positions = vec![];
+    match mv.0 {
+        Direction::Up => {
+            let new_pos_y = points_pos[0].0 + mv.1;
+            while points_pos[0].0 != new_pos_y {
+                points_pos[0].0 += 1;
+                follow_chain(points_pos);
+                tail_positions.push(points_pos[points_pos.len() - 1]);
+            }
         }
-        tail_pos.0 = head_pos.0;
-        tail_pos.1 = head_pos.1 - 1;
-    }
-
-    return visited;
-}
-
-fn move_down(head_pos: &mut (i32, i32), tail_pos: &mut (i32, i32), d: i32) -> Vec<(i32, i32)> {
-    let mut visited = vec![];
-    head_pos.1 -= d;
-    let distance = dist(head_pos, tail_pos);
-    if distance > 1 {
-        for i in head_pos.1 + 1..tail_pos.1 {
-            visited.push((head_pos.0, i));
+        Direction::Down => {
+            let new_pos_y = points_pos[0].0 - mv.1;
+            while points_pos[0].0 != new_pos_y {
+                points_pos[0].0 -= 1;
+                follow_chain(points_pos);
+                tail_positions.push(points_pos[points_pos.len() - 1]);
+            }
         }
-        tail_pos.0 = head_pos.0;
-        tail_pos.1 = head_pos.1 + 1;
+        Direction::Right => {
+            let new_pos_x = points_pos[0].1 + mv.1;
+            while points_pos[0].1 != new_pos_x {
+                points_pos[0].1 += 1;
+                follow_chain(points_pos);
+                tail_positions.push(points_pos[points_pos.len() - 1]);
+            }
+        }
+        Direction::Left => {
+            let new_pos_x = points_pos[0].1 - mv.1;
+            while points_pos[0].1 != new_pos_x {
+                points_pos[0].1 -= 1;
+                follow_chain(points_pos);
+                tail_positions.push(points_pos[points_pos.len() - 1]);
+            }
+        }
     }
 
-    return visited;
-}
-
-fn make_move(
-    mut head_pos: (i32, i32),
-    mut tail_pos: (i32, i32),
-    mv: (Direction, i32),
-    total_visited: &mut HashSet<(i32, i32)>,
-) -> ((i32, i32), (i32, i32)) {
-    let now_visited = match mv.0 {
-        Direction::Up => move_up(&mut head_pos, &mut tail_pos, mv.1),
-        Direction::Down => move_down(&mut head_pos, &mut tail_pos, mv.1),
-        Direction::Left => move_left(&mut head_pos, &mut tail_pos, mv.1),
-        Direction::Right => move_right(&mut head_pos, &mut tail_pos, mv.1),
-    };
-
-    for point in now_visited {
-        total_visited.insert(point);
-    }
-
-    (head_pos, tail_pos)
+    return tail_positions;
 }
 
 fn part1(content: String) {
     let mut visited: HashSet<(i32, i32)> = HashSet::new();
     visited.insert((0, 0));
-    let mut head_position = (0, 0);
-    let mut tail_position = (0, 0);
+    let mut points_pos = [(0, 0), (0, 0)];
 
     let moves: Vec<(Direction, i32)> = parse_input(content);
     for mv in moves {
-        (head_position, tail_position) = make_move(head_position, tail_position, mv, &mut visited);
+        let tail_pos = move_v2(&mut points_pos, mv);
+        for pos in tail_pos {
+            visited.insert(pos);
+        }
     }
 
     println!("total visited: {}", visited.len());
 }
 
-// fn make_move_v2(
-//     points_pos: &mut [(i32, i32)],
-//     mv: (Direction, i32),
-//     total_visited: &mut HashSet<(i32, i32)>,
-// ) {
-//     for i in 0..points_pos.len() - 1 {
-//         let mut d = 0;
-//         if i == 0 {
-//             d = mv.1;
-//         }
+fn part2(content: String) {
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    visited.insert((0, 0));
+    let mut points_pos = [
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+    ];
+    let moves: Vec<(Direction, i32)> = parse_input(content);
+    for mv in moves {
+        let tail_pos = move_v2(&mut points_pos, mv);
+        for pos in tail_pos {
+            visited.insert(pos);
+        }
+    }
 
-//         let mut head = points_pos[i];
-//         let mut tail = points_pos[i + 1];
-
-//         let now_visited = match mv.0 {
-//             Direction::Up => move_up(&mut head, &mut tail, d),
-//             Direction::Down => move_down(&mut head, &mut tail, d),
-//             Direction::Left => move_left(&mut head, &mut tail, d),
-//             Direction::Right => move_right(&mut head, &mut tail, d),
-//         };
-
-//         points_pos[i] = head;
-//         points_pos[i + 1] = tail;
-
-//         //last pair - these are points visited by tail
-//         if i == points_pos.len() - 2 {
-//             for point in now_visited {
-//                 total_visited.insert(point);
-//                 println!("TAIL POINT: {:?}", point);
-//             }
-//         }
-//     }
-// }
-
-// fn part2(content: String) {
-//     let mut visited: HashSet<(i32, i32)> = HashSet::new();
-//     visited.insert((0, 0));
-//     let mut points_pos = [
-//         (0, 0),
-//         (0, 0),
-//         (0, 0),
-//         (0, 0),
-//         (0, 0),
-//         (0, 0),
-//         (0, 0),
-//         (0, 0),
-//         (0, 0),
-//         (0, 0),
-//     ];
-//     let moves: Vec<(Direction, i32)> = parse_input(content);
-//     for mv in moves {
-//         make_move_v2(&mut points_pos, mv, &mut visited);
-//         println!("{:?}", points_pos);
-//     }
-
-//     println!("total visited: {}", visited.len());
-// }
+    println!("total visited: {}", visited.len());
+}
 
 fn main() {
-    let content = fs::read_to_string("input2").expect("Failed to open file!");
+    let content = fs::read_to_string("input").expect("Failed to open file!");
     part1(content.clone());
-    // part2(content.clone());
+    part2(content.clone());
 }
